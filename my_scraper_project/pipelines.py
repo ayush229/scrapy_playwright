0cda9d5d94003a08221f53046d4ee942a0778f8a
ayush229 <53784806+ayush229@users.noreply.github.com>
2025-06-05 15:10:37 +0530

    Update pipelines.py

--- my_scraper_project/pipelines.py
+++ my_scraper_project/pipelines.py
@@ -1,16 +1,18 @@
 import json
 import logging
 from itemadapter import ItemAdapter
-from scrapy.utils.project import get_project_settings
+from scrapy.utils.project import get_project_settings # This might not be needed anymore if queue is passed directly
 
 logger = logging.getLogger(__name__)
 
 class JsonWriterPipeline:
     def open_spider(self, spider):
-        settings = get_project_settings()
-        self.results_queue = settings.get('SCRAPY_RESULTS_QUEUE')
+        # Now, the queue is expected to be set directly on the spider instance
+        # if it's passed during spider initialization.
+        # We no longer rely on get_project_settings for the queue.
+        self.results_queue = getattr(spider, 'results_queue', None) 
         if not self.results_queue:
-            spider.logger.error("SCRAPY_RESULTS_QUEUE not found in settings. Results will not be passed back to Flask.")
+            spider.logger.error("SCRAPY_RESULTS_QUEUE (results_queue) not found on spider instance. Results will not be passed back.")
         else:
             spider.logger.info("Scrapy results will be put into the shared queue.")
 
